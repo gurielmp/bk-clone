c899d8d9d1b64592ff8eb25667ca74b7c5760761
gurielamp <gurielamp@gmail.com>
2023-11-05 20:23:36 +0700

    styling and fix broken css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import NavBar from "@/components/NavBar/NavBar"
+import Footer from "@/components/Footer/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="flex min-h-screen flex-col items-center">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   )
